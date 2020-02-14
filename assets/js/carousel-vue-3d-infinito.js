d71cab83d64bf1d95ca5ff59c8c0a36116ecfa47
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2020-02-14 16:54:50 -0300

    Correção de bugs no frontend, e carregamento da história da cliente, na seção #história

--- assets/js/carousel-vue-3d-infinito.js
+++ assets/js/carousel-vue-3d-infinito.js
@@ -1,7 +1,73 @@
 new Vue({
    el: '#minha-equipe',
    data: {
-     slides: 7
+    slides: [
+      {
+        title: 'Mundo da Babi',
+        mainSrc: 'assets/img/nubia.jpg',
+        logoSrc: '',
+        href:'',
+        name: 'Jone',
+        desc: 'Frase ou curto texto relacionado ao funcionado em questão',
+        tag: 'Blog',
+      },{
+        title: 'Mundo da Babi',
+        mainSrc: 'assets/img/amigas.jpg',
+        logoSrc: '',
+        href:'',
+        name: 'Jone',
+        desc: 'Frase ou curto texto relacionado ao funcionado em questão',
+        tag: 'Blog',
+      },{
+        title: 'Mundo da Babi',
+        mainSrc: 'assets/img/familia.jpg',
+        logoSrc: '',
+        href:'',
+        name: 'Jone',
+        desc: 'Frase ou curto texto relacionado ao funcionado em questão',
+        tag: 'Blog',
+      },{
+        title: 'Mundo da Babi',
+        mainSrc: 'assets/img/familia2.jpg',
+        logoSrc: '',
+        href:'',
+        name: 'Jone',
+        desc: 'Frase ou curto texto relacionado ao funcionado em questão',
+        tag: 'Blog',
+      }, {
+        title: 'Imobiliária Vasconcelos',
+        mainSrc: 'assets/img/familia2.jpg',
+        logoSrc: '',
+        href:'',
+        name: 'Jone',
+        desc: 'Frase ou curto texto relacionado ao funcionado em questão',
+        tag: 'Site',
+      }, {
+        title: 'Kevin Arquitetura',
+        mainSrc: 'assets/img/familia1.jpg',
+        logoSrc: '',
+        href:'',
+        name: 'Jone',
+        desc: 'Frase ou curto texto relacionado ao funcionado em questão',
+        tag: 'Site',
+      }, {
+        title: 'Mundo da Babi',
+        mainSrc: 'assets/img/familia2.jpg',
+        logoSrc: '',
+        href:'',
+        name: 'Jone',
+        desc: 'Frase ou curto texto relacionado ao funcionado em questão',
+        tag: 'Blog',
+      }, {
+        title: 'Mundo da Babi',
+        mainSrc: 'assets/img/casal-evento.jpeg',
+        logoSrc: '',
+        href:'',
+        name: 'Jone',
+        desc: 'Frase ou curto texto relacionado ao funcionado em questão',
+        tag: 'Blog',
+      }
+    ]
    },
    components: {
      'carousel-3d': Carousel3d.Carousel3d,
